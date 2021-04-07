68d4ac25b33507e0bd8ca744c03aabb1ea0fc09b
Alexander Oldenstam <alxr.andr@gmail.com>
2021-04-08 01:56:06 +0200

    Fixed voicestate status

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ client.login(config.BOT_TOKEN)
 client.on("voiceStateUpdate", (oldVoiceState, newVoiceState) => { // Listeing to the voiceStateUpdate event
   if (newVoiceState.channel) { // The member connected to a channel.
 
-    if (member.oldVoiceState == "791047918483537924" ) {
+    if (member.oldVoiceState == newVoiceState.channel ) {
 
       console.log("Already in the voice channel\nProbably muting/unmuting");
     } else {
